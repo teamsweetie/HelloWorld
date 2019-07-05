1af32200c35aff7223b8079037d79fd79d25b81b
ursus-code <52512140+ursus-code@users.noreply.github.com>
2019-07-05 17:43:39 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+	   Junk myJunk = new Junk();
+	      System.out.println("Hello, World");
+	      System.out.println(myJunk.x);
+   }
+}
---
+++ Junk.java
@@ -0,0 +1,5 @@
+
+public class Junk {
+   int x = 5;
+  
+}
