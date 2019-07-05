b969b0a7c6be503ba24dcd6cfbb635d82d0db23f
ursus-code <ursus@alum.mit.edu>
2019-07-05 18:00:26 -0400

    try2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
 	   Junk myJunk = new Junk();
-	      System.out.println("Hello, World");
+	      System.out.println("Hello, World-GitHub");
 	      System.out.println(myJunk.x);
    }
 }
